b72176e0e358f41d5df27b218d8017f74be16a80
Sajarin M <sajarinm@gmail.com>
2023-11-29 00:13:20 +0530

    finish sidebar layout

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Code, Group } from '@mantine/core';
+import { useLocation } from 'react-router-dom';
+import { Code } from '@mantine/core';
 import {
   Icon2fa,
   IconBellRinging,
@@ -9,72 +9,69 @@ import {
   IconLogout,
   IconReceipt2,
   IconSettings,
-  IconSwitchHorizontal,
 } from '@tabler/icons-react';
 
-const data = [
-  { link: '', label: 'Notifications', icon: IconBellRinging },
-  { link: '', label: 'Billing', icon: IconReceipt2 },
-  { link: '', label: 'Security', icon: IconFingerprint },
-  { link: '', label: 'SSH Keys', icon: IconKey },
-  { link: '', label: 'Databases', icon: IconDatabaseImport },
-  { link: '', label: 'Authentication', icon: Icon2fa },
-  { link: '', label: 'Other Settings', icon: IconSettings },
+const sidebarItems = [
+  { href: '', label: 'Notifications', icon: IconBellRinging },
+  { href: '', label: 'Billing', icon: IconReceipt2 },
+  { href: '', label: 'Security', icon: IconFingerprint },
+  { href: '', label: 'SSH Keys', icon: IconKey },
+  { href: '', label: 'Databases', icon: IconDatabaseImport },
+  { href: '', label: 'Authentication', icon: Icon2fa },
+  { href: '', label: 'Other Settings', icon: IconSettings },
 ];
 export default function Pages() {
-  const [active, setActive] = useState('Billing');
-
-  const links = data.map((item) => (
-    <a
-      className='text-gray-7 hover:bg-gray-0 data-[active]:bg-blue data-[active]:text-blue  dark:text-dark-1 dark:hover:bg-dark-6 px-sm py-xs group flex items-center rounded-sm text-sm font-medium no-underline hover:text-black dark:hover:text-white'
-      data-active={item.label === active || undefined}
-      href={item.link}
-      key={item.label}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(item.label);
-      }}
-    >
-      <item.icon className='mr-sm text-gray-6 dark:text-dark-2 h-[25px] w-[25px]' stroke={1.5} />
-      <span>{item.label}</span>
-    </a>
-  ));
+  const { pathname } = useLocation();
 
   return (
-    <nav className='d-flex border-gray-3 p-md dark:border-dark-4 h-screen w-[300px] flex-col border-r border-solid'>
+    <nav className='border-default-border p-md flex h-screen w-[17.5rem] flex-col border-r border-solid'>
       <div className='flex-grow'>
-        <Group
-          className='border-gray-3 dark:border-dark-4 mb-6 border-b border-solid pb-4'
-          justify='space-between'
-        >
+        <div className='border-default-border pb-md mb-md flex items-center justify-between border-b border-solid'>
           {/* <MantineLogo size={28} /> */}
-          <Code fw={700}>v3.1.2</Code>
-        </Group>
-        {links}
+          <Code fw={700}>v1.0.0</Code>
+        </div>
+        {sidebarItems.map((item) => {
+          const isActive = item.href === pathname;
+          return (
+            <SidebarItem
+              icon={item.icon}
+              href={item.href}
+              key={item.label}
+              label={item.label}
+              isActive={isActive ? true : undefined}
+            />
+          );
+        })}
       </div>
 
-      <div className='border-gray-3 dark:border-dark-4 mt-4 border-t border-solid pt-4'>
-        <a
-          href='#'
-          className='text-gray-7 hover:bg-gray-0 data-[active]:bg-blue data-[active]:text-blue dark:text-dark-1 dark:hover:bg-dark-6 group flex items-center rounded-sm px-2 py-1 text-sm font-medium no-underline hover:text-black dark:hover:text-white'
-          onClick={(event) => event.preventDefault()}
-        >
-          <IconSwitchHorizontal className='group-hover:text-black' stroke={1.5} />
-          <span>Change account</span>
-        </a>
-
-        <a
-          href='#'
-          className='text-gray-7 hover:bg-gray-0 data-[active]:bg-blue data-[active]:text-blue  dark:text-dark-1 dark:hover:bg-dark-6 group flex items-center rounded-sm px-2 py-1 text-sm font-medium no-underline hover:text-black dark:hover:text-white'
-          onClick={(event) => event.preventDefault()}
-        >
-          <IconLogout
-            className='mr-sm text-gray-6 dark:text-dark-2 h-[25px] w-[25px]'
-            stroke={1.5}
-          />
-          <span>Logout</span>
-        </a>
+      <div className='border-default-border mt-md pt-md border-t border-solid'>
+        <SidebarItem icon={IconLogout} href='/logout' label='Logout' />
       </div>
     </nav>
+  );
+}
+
+type SidebarItemProps = {
+  isActive?: boolean;
+  href: string;
+  icon: React.FC<{ className?: string; stroke?: number }>;
+  label: string;
+};
+
+function SidebarItem({ href, icon: Icon, label, isActive }: SidebarItemProps) {
+  return (
+    <a
+      data-active={isActive}
+      className={
+        'text-gray-7 gap-sm dark:text-dark-1 px-sm py-xs hover:bg-gray-0 dark:hover:bg-dark-6 data-[active]:text-primary-light-color data-[active]:hover:text-primary-light-color data-[active]:bg-primary-light data-[active]:hover:bg-primary-light group flex items-center rounded-sm text-sm font-semibold no-underline hover:text-black dark:hover:text-white'
+      }
+      href={href}
+    >
+      <Icon
+        stroke={1.5}
+        className='group-data-[active]:text-primary-light-color group-hover:text-black dark:group-hover:text-white'
+      />
+      <span>{label}</span>
+    </a>
   );
 }
