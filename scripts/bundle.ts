8db971a798ed5a5621f687ab4b5bc15393460121
Sajarin M <sajarinm@gmail.com>
2024-04-12 21:02:27 +0530

    organize bundle

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -14,14 +14,3 @@ await $`cp -r ${serverPath}/dist  .`;
 await $`cp -r ${serverPath}/prisma  ./dist`;
 await $`cp -r ${webPath}/dist  ./dist`;
 await $`mv ./dist/dist ./dist/static`;
-
-const env = `NODE_ENV=production
-PORT=3001
-DATABASE_URL=postgresql://postgres:yEjJFzZJ0M5wpDh@db:5432/rent_house?schema=public
-STATIC_DIRECTORY=/rent-house/scripts/dist/static
-IMAGES_DIRECTORY=/rent-house-data/images
-PASSWORD_FILE=/rent-house-data/password.txt
-AUTO_MIGRATE=true
-`;
-
-await $`echo ${env} > ./dist/.env`;
